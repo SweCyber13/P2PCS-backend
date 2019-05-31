2d0c7f6276d7a6a9291a0c8af636abb4fda06f31
Andrea Casagrande <esgroth@gmail.com>
2019-05-31 14:56:33 +0200

    Create funzioni per eventi in event.php

--- Api/objects/event.php
+++ Api/objects/event.php
@@ -23,22 +23,121 @@ class event
     }
 
     function read() {
+        // select all query
+        $query = "SELECT * FROM $this->table_name WHERE Id_eventi=?";
 
+        // prepare query statement
+        $stmt = $this->conn->prepare($query);
+
+        //bind params
+        $stmt->bind_param("i",$this->id);
+
+        // execute query
+        $stmt->execute();
+
+        //get select results
+        $result=$stmt->get_result();
+
+        return $result;
     }
 
     function readall() {
+        // select all query
+        $query = "SELECT * FROM $this->table_name";
 
+        // prepare query statement
+        $stmt = $this->conn->prepare($query);
+
+        // execute query
+        $stmt->execute();
+
+        //get select results
+        $result=$stmt->get_result();
+
+        return $result;
     }
 
     function delete(){
+        // select all query
+        $query = "DELETE FROM $this->table_name WHERE Id_eventi=?";
 
+        // prepare query statement
+        $stmt = $this->conn->prepare($query);
+
+        //bind params
+        $stmt->bind_param("i",$this->username);
+
+        // execute query
+        $result=$stmt->execute();
+
+        return $result;
     }
 
     function create(){
+        // select all query
+        $query = "INSERT INTO $this->table_name(Id_eventi, Nome_azienda, Titolo_offerta, Descrizione)  VALUES(?, ?, ?, ?)";
 
+        // prepare query statement
+        $stmt = $this->conn->prepare($query);
+
+        //bind params
+        $stmt->bind_param("isss",$this->username,$this->nome,$this->cognome,$this->mail);
+
+        // execute query and save success or error
+        $result=$stmt->execute();
+
+        return $result;
     }
 
     function update(){
+        //check what params need to be updated
+        //$fields="";
+        $errors=0;
+        if($this->nome_azienda) {
+            $query="UPDATE $this->table_name SET Nome_azienda=? WHERE Id_eventi=?";
+            // prepare query statement
+            $stmt = $this->conn->prepare($query);
+
+            //bind params
+            $stmt->bind_param("si",$this->nome_azienda,$this->id);
+
+            // execute query and save success or error
+            if(!$stmt->execute()) {
+                $errors=$errors+1;
+            }
+
+        }
+        if ($this->titolo_offerta){
+            $query="UPDATE $this->table_name SET Titolo_offerta=? WHERE Id_eventi=?";
+            // prepare query statement
+            $stmt = $this->conn->prepare($query);
+
+            //bind params
+            $stmt->bind_param("si",$this->titolo_offerta,$this->id);
+
+            // execute query and save success or error
+            if(!$stmt->execute()) {
+                $errors=$errors+1;
+            }
+        }
+        if($this->descrizione){
+            $query="UPDATE $this->table_name SET Descrizione=? WHERE Id_eventi=?";
+            // prepare query statement
+            $stmt = $this->conn->prepare($query);
+
+            //bind params
+            $stmt->bind_param("si",$this->descrizione,$this->id);
+
+            // execute query and save success or error
+            if(!$stmt->execute()) {
+                $errors=$errors+1;
+            }
+        }
+
+        //return 0 if there was an error
+        if($errors>0) return false;
+        else
+            return true;
 
     }
 }
